9ee5a26cdb039bd26e0a9e30b2394389ed384a46
zainabasifali <zainabasif5312@gmail.com>
2026-03-09 15:34:20 +0500

    render added

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   preview: {
-    host: true,
+  preview: {
+    host: '0.0.0.0',
     port: process.env.PORT,
-    allowedHosts: ["mps-frontend-b7qd.onrender.com"]
+    allowedHosts: true
   }
 })
